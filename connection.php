714f511e9fe1d0ff791cbed578fec6649d7f1452
sagarhirapara <86096538+sagarhirapara@users.noreply.github.com>
2021-07-04 13:21:42 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root',"","swaminarayan") or die("database connection failed");
-// $conn = mysqli_connect('sql113.epizy.com','epiz_29022678',"FSxLomd6DCUUqV7","epiz_29022678_swaminarayan") or die("database connection failed");
-?>
